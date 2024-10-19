df6a015b246cac4ce9faf574e2443a6d1b449ca0
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2024-10-19 19:53:44 +0300

    ready

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,14 @@
 export const refs = {
-  gallery: document.querySelector('.gallery'),
-  form: document.querySelector('.query-form'),
-  loader: document.querySelector('.loader'),
+  gallery     : document.querySelector('.gallery'),
+  form        : document.querySelector('.query-form'),
+  loader      : document.querySelector('.loader'),
+  loadBtn     : document.querySelector('.load-btn'),
+  endWarningText: document.querySelector('.end-warning'),
 };
 
-export const PX_API_KEY="28534332-8f968f9e2a1846e3bb62dda3d"
+export const PX_API_KEY = '28534332-8f968f9e2a1846e3bb62dda3d';
 
-export const STORAGE_KEY = "query";
+export const STORAGE_KEY = 'query';
 
 export const simpleLightboxOptions = {
   overlay        : true,
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,10 +1,11 @@
-export function renderGallery(cards) {
-  return cards
+export function renderGallery(photos) {
+
+  return photos
     .map(({ webformatURL, tags, likes, views, comments, downloads, largeImageURL }) =>
-        `
+      `
             <li class="gallery-card">
                 <a class="gallery-link" href="${largeImageURL}">
-                    <img class="gallery-image" src="${webformatURL}" alt="${tags}" height=312 width=200>
+                    <img class="gallery-image" src="${webformatURL}" alt="${tags}" height="312" width="200" loading="lazy">
                     <ul class="statistics">
                         <li class="stat-element">
                             <p class="stat-name">Likes</p>
@@ -28,7 +29,7 @@ export function renderGallery(cards) {
                     </ul>
                 </a>
             </li>
-      `
+      `,
     )
-    .join("");
+    .join('');
 }
